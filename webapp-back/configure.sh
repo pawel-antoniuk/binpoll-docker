a39b43606fb713103f43e0f735f7f64898424a55
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-06-15 12:56:15 +0200

    simplified container

--- webapp-back/configure.sh
+++ webapp-back/configure.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd "$BINPOLL_BACK_SRC"/binpoll_back \
+cd "$BINPOLL_BACK_SRC" \
     && . /app/venv/bin/activate \
     && python manage.py collectstatic --noinput \
     && python manage.py makemigrations data_collector \
     && python manage.py migrate \
     && cd /app && python populate_db.py \
-    && ln -s "$BINPOLL_BACK_SRC"/binpoll_back "$BINPOLL_BACK_TARGET" \
-    && ln -s /app/poll_sounds "$BINPOLL_BACK_SRC"/binpoll_back/static \
+    && ln -sf "$BINPOLL_BACK_SRC" "$BINPOLL_BACK_TARGET" \
+    && ln -sf /app/poll_sounds "$BINPOLL_BACK_SRC"/static \
     && a2enmod headers \
     && a2enmod ratelimit \
     && a2dissite 000-default \
